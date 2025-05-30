7bb7e68c0e82821c91bbce6e73bacade9d82aa66
linke <linke@duck.com>
2025-05-31 01:15:50 +0300

    Complete Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    // task 1
+
+    public static void isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        int year = 2021;
+        isLeapYear(year);
+    }
 }
